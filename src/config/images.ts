e978f09564fa02f9ff3d14c1556899681f87d0f5
webdragon0125 <jakus.superdev@gmail.com>
2022-12-21 01:19:59 +0100

    Pair icons

--- src/config/images.ts
+++ src/config/images.ts
@@ -9,14 +9,25 @@ import MetamaskSvg from '../assets/icons/metamask.svg';
 import CopySvg from '../assets/icons/copy.svg';
 import ExpandSvg from '../assets/icons/expand.svg';
 import GasStationSvg from '../assets/icons/gas-station.svg';
-import BitcoinSvg from '../assets/images/CoinImage/BitcoinLogo.svg';
-import EtherLogoSvg from '../assets/images/CoinImage/EtherLogo.svg';
-import LunaLogoSvg from '../assets/images/CoinImage/LunaLogo.svg';
-import MinexLogo from '../assets/images/CoinImage/MinexLogo.svg';
-import XrpLogoSvg from '../assets/images/CoinImage/XrpLogo.svg';
 
 // Coins & Tokens
-import BtcImg from '../assets/images/btc.png';
+import adaLogo from '../assets/images/CoinImage/adaLogo.svg';
+import algoLogo from '../assets/images/CoinImage/algoLogo.svg';
+import atomLogo from '../assets/images/CoinImage/atomLogo.svg';
+import avaxLogo from '../assets/images/CoinImage/avaxLogo.svg';
+import bchLogo from '../assets/images/CoinImage/bchLogo.svg';
+import btcLogo from '../assets/images/CoinImage/btcLogo.svg';
+import bnbLogo from '../assets/images/CoinImage/bnbLogo.svg';
+import dogeLogo from '../assets/images/CoinImage/dogeLogo.svg';
+import dotLogo from '../assets/images/CoinImage/dotLogo.svg';
+import ethLogo from '../assets/images/CoinImage/ethLogo.svg';
+import linkLogo from '../assets/images/CoinImage/linkLogo.svg';
+import ltcLogo from '../assets/images/CoinImage/ltcLogo.svg';
+import maticLogo from '../assets/images/CoinImage/maticLogo.svg';
+import solLogo from '../assets/images/CoinImage/solLogo.svg';
+import uniLogo from '../assets/images/CoinImage/uniLogo.svg';
+import xmrLogo from '../assets/images/CoinImage/xmrLogo.svg';
+import xrpLogo from '../assets/images/CoinImage/xrpLogo.svg';
 
 export {
   LOGO,
@@ -30,10 +41,21 @@ export {
   CopySvg,
   ExpandSvg,
   GasStationSvg,
-  BtcImg,
-  BitcoinSvg,
-  EtherLogoSvg,
-  LunaLogoSvg,
-  MinexLogo,
-  XrpLogoSvg
+  adaLogo,
+  algoLogo,
+  atomLogo,
+  avaxLogo,
+  bchLogo,
+  btcLogo,
+  bnbLogo,
+  dogeLogo,
+  dotLogo,
+  ethLogo,
+  linkLogo,
+  ltcLogo,
+  maticLogo,
+  solLogo,
+  uniLogo,
+  xmrLogo,
+  xrpLogo
 };
