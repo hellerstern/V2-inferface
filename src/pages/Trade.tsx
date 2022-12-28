4b80ba9def1b7f2ff77ddc2ce1a9fb2d1d173636
webdragon0125 <jakus.superdev@gmail.com>
2022-12-29 01:03:59 +0900

    highchart scroll style is changed

--- src/pages/Trade.tsx
+++ src/pages/Trade.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useContext } from 'react';
+import { useEffect, useState } from 'react';
 import { Box } from '@mui/material';
 import { styled } from '@mui/system';
 import { Container } from '../../src/components/Container';
