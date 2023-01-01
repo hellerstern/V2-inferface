f112ad5448cafaade909263c31f6b23635718205
webdragon0125 <jakus.superdev@gmail.com>
2023-01-01 05:58:09 +0100

    More image searches

--- src/context/profile.js
+++ src/context/profile.js
@@ -36,7 +36,7 @@ async function generateUsername() {
 }
 
 async function getRandomCatgirl() {
-    const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=AIzaSyBiPxAr2gmWpR4d9Vxt_tZaeIJf-XH0jn4&cx=e0f354ced324a40e9&q=anime+catgirl+profile+picture&searchType=image&start=${Math.floor(Math.random() * 100)}`);
+    const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=AIzaSyBiPxAr2gmWpR4d9Vxt_tZaeIJf-XH0jn4&cx=e0f354ced324a40e9&q=anime+catgirl+profile+picture&searchType=image&start=${Math.floor(Math.random() * 180)}`);
     const data = await response.json();
     const image = data.items[0];
     return image.link;
