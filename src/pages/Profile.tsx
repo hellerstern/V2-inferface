aa1bb53b4add1c0830e8e3cc955e1d1142c7ca08
webdragon0125 <jakus.superdev@gmail.com>
2023-01-06 06:25:00 +0900

    profile page layout is changed

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,73 +1,87 @@
 import { useState } from 'react';
-import { Box, Button, FormControlLabel, FormGroup, Divider } from '@mui/material';
+import { Box, Divider, IconButton, Avatar, Badge } from '@mui/material';
+import { AddPhotoAlternate } from '@mui/icons-material';
 import { styled } from '@mui/system';
 import { Container } from 'src/components/Container';
-import { ArbiScanSvg, LOGO, PolygonSvg } from 'src/config/images';
-import { TigrisCheckBox } from 'src/components/CheckBox';
 import './css/profile.css';
 import { TraderProfile } from 'src/context/profile';
-import { TigrisDatePicker } from '../../src/components/DatePicker';
 import { Dayjs } from 'dayjs';
 
 export const Profile = () => {
-
   const [value, setValue] = useState<Dayjs | null>(null);
+  const [avatar, setAvatar] = useState(TraderProfile().profilePicture);
+  const handleChange = (event: any) => {
+    if (event.target.files.length > 0) {
+      const choosenImg = URL.createObjectURL(event.target.files[0]);
+      setAvatar(choosenImg);
+    }
+  };
 
   return (
     <Container>
       <ProfileContainer>
         <Wrapper>
-          <SectionHeader>
-            Trader Profile
-          </SectionHeader>
-          <SectionContent>
-            <div style={{ display: 'flex', marginBottom: 20, marginTop: 5 }}>
-              <div>
-                <div className="profileBorder" style={{position: 'relative'}}/>
-                <img src={TraderProfile().profilePicture} style={{borderRadius: '9999px', width: '100px', height: '100px', position: 'absolute', marginTop: -100}} />
-              </div>
-              <div style={{ marginLeft: 50}}>
-                <div style={{ display: 'flex', alignItems: 'center' }}>
-                  <h4 style={{ margin: 0, fontWeight: 'normal', fontSize: '50px' }}>{TraderProfile().username}</h4>
-                </div>
-                <p style={{ margin: 0, whiteSpace: 'pre-wrap', fontSize: '13px' }}>{TraderProfile().description}</p>
-              </div>
-            </div>
-          </SectionContent>
-          <SectionHeader>
-            Public Stats
-          </SectionHeader>
-          <OverviewWrapper>
-            <OverviewContent>
-              <OverviewSeparator>
-                <OverviewItem title={'Total PNL ($)'} value="12300.45" colorValue="rgba(255, 255, 255, 0.6)" />
-                <OverviewItem title={'Biggest win ($)'} value="6000.23" colorValue="rgba(255, 255, 255, 0.6)" />
-                <OverviewItem title={'Biggest win %'} value="453.15%" colorValue="rgba(255, 255, 255, 0.6)" />
-                <OverviewItem
-                  title={'Total trades'}
-                  value="1243"
-                  colorValue="rgba(255, 255, 255, 0.6)"
-                />
-                <OverviewItem
-                  title={'Winrate %'}
-                  value="52.7%"
-                  colorValue="rgba(255, 255, 255, 0.6)"
-                />
-              </OverviewSeparator>
-              <ResponsiveDevider1 />
-              <ResponsiveDevider2 />
-              <OverviewSeparator>
-                <OverviewItem title={'Take profit price'} value="0.34789247" colorValue="#58BD7D" />
-                <OverviewItem title={'Entry price'} value="0.29039402" colorValue="rgba(255, 255, 255, 0.6)" />
-                <OverviewItem title={'Exit Price'} value="0.4598948585" colorValue="rgba(255, 255, 255, 0.6)" />
-                <OverviewItem title={'Stop limit price'} value="0.4590594" colorValue="rgba(255, 255, 255, 0.6)" />
-                <OverviewItem title={'Liq price'} value="0.3840934804" colorValue="#D33535" />
+          <ProfileSection>
+            {/* <SectionHeader>Trader Profile</SectionHeader> */}
+            <SectionContent>
+              <AvatarUpload>
+                <input type="file" onChange={handleChange} id="upload" accept="image/*" style={{ display: 'none' }} />
+                <label htmlFor="upload">
+                  <IconButton color="primary" aria-label="upload picture" component="span">
+                    <Badge
+                      overlap="circular"
+                      anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+                      badgeContent={
+                        <Avatar sx={{ bgcolor: '#D9DBDC', width: '35px', height: '35px' }}>
+                          <AddPhotoAlternate sx={{ color: '#555961' }} />
+                        </Avatar>
+                      }
+                    >
+                      <Avatar
+                        id="avatar"
+                        src={avatar}
+                        style={{
+                          width: '150px',
+                          height: '150px'
+                        }}
+                      />
+                    </Badge>
+                  </IconButton>
+                </label>
+                <label htmlFor="avatar" />
+              </AvatarUpload>
+              <UserDetail>
+                <UserName>{TraderProfile().username}</UserName>
+                <UserDescription>{TraderProfile().description}</UserDescription>
+              </UserDetail>
+            </SectionContent>
+          </ProfileSection>
+          <OverviewSection>
+            {/* <SectionHeader>Public Stats</SectionHeader> */}
+            <OverviewWrapper>
+              <OverviewContent>
+                <OverviewSeparator>
+                  <OverviewItem title={'Total PNL ($)'} value="12300.45" colorValue="rgba(255, 255, 255, 0.6)" />
+                  <OverviewItem title={'Biggest win ($)'} value="6000.23" colorValue="rgba(255, 255, 255, 0.6)" />
+                  <OverviewItem title={'Biggest win %'} value="453.15%" colorValue="rgba(255, 255, 255, 0.6)" />
+                  <OverviewItem title={'Total trades'} value="1243" colorValue="rgba(255, 255, 255, 0.6)" />
+                  <OverviewItem title={'Winrate %'} value="52.7%" colorValue="rgba(255, 255, 255, 0.6)" />
+                </OverviewSeparator>
+                <ResponsiveDevider1 />
                 <ResponsiveDevider2 />
-                <OverviewItem title={'Total volume traded'} value="8,3million" colorValue="#777E90" />
-                <OverviewItem title={'Number of trades'} value="Number of trades" colorValue="#777E90" />
-              </OverviewSeparator>
-            </OverviewContent>
-          </OverviewWrapper>
+                <OverviewSeparator>
+                  <OverviewItem title={'Take profit price'} value="0.34789247" colorValue="#58BD7D" />
+                  <OverviewItem title={'Entry price'} value="0.29039402" colorValue="rgba(255, 255, 255, 0.6)" />
+                  <OverviewItem title={'Exit Price'} value="0.4598948585" colorValue="rgba(255, 255, 255, 0.6)" />
+                  <OverviewItem title={'Stop limit price'} value="0.4590594" colorValue="rgba(255, 255, 255, 0.6)" />
+                  <OverviewItem title={'Liq price'} value="0.3840934804" colorValue="#D33535" />
+                  <ResponsiveDevider2 />
+                  <OverviewItem title={'Total volume traded'} value="8,3million" colorValue="#777E90" />
+                  <OverviewItem title={'Number of trades'} value="Number of trades" colorValue="#777E90" />
+                </OverviewSeparator>
+              </OverviewContent>
+            </OverviewWrapper>
+          </OverviewSection>
         </Wrapper>
       </ProfileContainer>
     </Container>
@@ -85,10 +99,20 @@ const ProfileContainer = styled(Box)(({ theme }) => ({
 const Wrapper = styled(Box)(({ theme }) => ({
   width: '100%',
   display: 'flex',
-  flexDirection: 'column',
   gap: '12px',
-  alignItems: 'center',
   maxWidth: '960px'
+}));
+
+const ProfileSection = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '12px'
+}));
+
+const OverviewSection = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  width: '100%'
 }));
 
 const SectionHeader = styled(Box)(({ theme }) => ({
@@ -108,7 +132,27 @@ const SectionHeader = styled(Box)(({ theme }) => ({
 const SectionContent = styled(Box)(({ theme }) => ({
   width: '100%',
   padding: '22px 27px',
-  backgroundColor: '#18191D'
+  backgroundColor: '#18191D',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  gap: '20px'
+}));
+
+const AvatarUpload = styled(Box)(({ theme }) => ({}));
+
+const UserDetail = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center'
+}));
+
+const UserName = styled(Box)(({ theme }) => ({
+  fontSize: '30px'
+}));
+
+const UserDescription = styled(Box)(({ theme }) => ({
+  fontSize: '13px'
 }));
 
 const ItemTitle = styled(Box)(({ theme }) => ({
