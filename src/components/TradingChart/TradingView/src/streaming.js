8850d6ab1e5bbac629791c8dce42d7baa987b628
webdragon0125 <jakus.superdev@gmail.com>
2023-01-23 01:25:06 +0100

    Chart css

--- src/components/TradingChart/TradingView/src/streaming.js
+++ src/components/TradingChart/TradingView/src/streaming.js
@@ -11,6 +11,9 @@ oracleSocket.on('data', (data) => {
   var tNow = new Date().getTime();
   const tradeTime = tNow - (tNow % 60000);
 
+  if (lastDailyBar === undefined || callback === undefined) {
+    return;
+  }
   const nextDailyBarTime = getNextDailyBarTime(lastDailyBar.time);
 
   let bar;
