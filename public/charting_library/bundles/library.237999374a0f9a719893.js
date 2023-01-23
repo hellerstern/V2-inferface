0a558f5622f292ca8f79dfe2b710c328dc0054fe
webdragon0125 <jakus.superdev@gmail.com>
2023-01-23 02:02:36 +0100

    Make prev pair expired on pair switch

--- public/charting_library/bundles/library.237999374a0f9a719893.js
+++ public/charting_library/bundles/library.237999374a0f9a719893.js
@@ -26489,6 +26489,7 @@
               return this._activeChangedChangedDelegate;
             }
             changeSymbol(e, t, i) {
+              this._symbolInfo.expired = true;
               r.interval.setValue(b.normalize(t)),
                 r.symbol.setValue(e),
                 i && this.activeChart().onDataLoaded().subscribe(null, i, !0);
